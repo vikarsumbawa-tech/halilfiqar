3089d2844f0a9d78e7811f80a936b13eb089656b
Synzz14 <vikar.sumbawa@gmail.com>
2025-11-14 23:53:21 +0800

    NIIII

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/home', [BlogController::class, 'home']);
+Route::get('/post/{id}', [BlogController::class, 'show']);
+Route::get('/about', [BlogController::class, 'about']);
+
